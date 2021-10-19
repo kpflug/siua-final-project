4f10e7c4a83cce5b61722e0d7dca64f3fed8ba76
Kristina Pflug <kpflug@xpanxion.com>
2021-10-19 13:51:53 -0400

    changes for final code review

--- UIM_Aurora.py
+++ UIM_Aurora.py
@@ -1,36 +1,15 @@
 import pymysql
 
-def main():
-    while True:
-        try: #Validating Customer ID
-           customerID = int(input("Enter your customer ID: "))
-        except ValueError:
-            print('Enter valid ID number')
-            continue
-        try:
-            username = input("Enter first and last name: ") 
-            validate_username(username) #Validation function
-        except ValueError:
-            print('Enter valid username')
-            continue
-        try:
-            quantity = int(input("Quantity of items: "))
-            if quantity > 1000:  # Checks if its an integer or if > 1000
-                raise ValueError
-        except ValueError:
-            print('Enter valid quantity')
-            continue
-        insertCustomer(customerID, username, quantity)
 
 def validate_username(username):
-    ''' Username validation. Checks for empty input, numeric input and spaces'''
+    ''' Username validation which checks for empty input, numeric input, and spaces '''
     if username.isnumeric() or len(username) < 1 or username.isspace():
         raise ValueError
     if all(x.isalpha() or x.isspace() for x in username):
         pass
 
 def insertCustomer(customerID, username, quantity):
-    '''Inserting Customer's input to TEAM-B DB table'''
+    ''' Inserts each customer's input into the TeamB database table '''
     retval = 0
     conn = createConnection()
     with conn.cursor() as cursor:
@@ -38,9 +17,9 @@ def insertCustomer(customerID, username, quantity):
         retval = cursor.lastrowid
     conn.commit()
     return retval
-
+    
 def createConnection():
-    '''Established connection to Amazon Aurora '''
+    ''' Establishes a connection to Amazon Aurora '''
     retval = None  
     USER = 'sia-db-user'
     HOST = 'sia-db-cluster-instance-1.cosgu9wr5iwp.us-east-1.rds.amazonaws.com'
@@ -52,6 +31,30 @@ def createConnection():
         password = PWD,
         database = DB)
     return retval
-    
+
+def main():
+    # Uses a WHILE LOOP with 'try' and 'except' for validation
+    while True:
+        try: 
+           customerID = int(input("Enter your customer ID: "))
+        except ValueError:
+            print('Enter valid ID number.')
+            continue
+        try:
+            username = input("Enter first and last name: ") 
+            validate_username(username) 
+        except ValueError:
+            print('Enter valid username.')
+            continue
+        try:
+            quantity = int(input("Quantity of items: "))
+            if quantity > 1000: 
+                raise ValueError
+        except ValueError:
+            print('Enter valid quantity.')
+            continue
+        insertCustomer(customerID, username, quantity)
+
+
 if __name__ == "__main__":
     main()
